5e155166e02c499952c4c4d0269fb6373bca4a30
swordgodwater-beep <swordgodwater@gmail.com>
2025-11-22 15:06:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188'] //['628','628'] 2 owner atau lebih
-global.author = 'Nazedev'
-global.botname = 'Hitori Bot'
+global.owner = ['6287760563021'] //['628','628'] 2 owner atau lebih
+global.author = 'Reyyz'
+global.botname = 'ReyzBotZ'
 global.packname = 'Bot WhatsApp'
 global.listprefix = ['+','!','.']
 
@@ -45,13 +45,13 @@ global.my = {
 global.limit = {
 	free: 20,
 	premium: 999,
-	vip: 9999
+	vip: 9999999999
 }
 
 global.money = {
 	free: 10000,
 	premium: 1000000,
-	vip: 10000000
+	vip: 9999999999
 }
 
 global.mess = {
@@ -90,3 +90,4 @@ fs.watchFile(file, () => {
 	delete require.cache[file]
 	require(file)
 });
+
